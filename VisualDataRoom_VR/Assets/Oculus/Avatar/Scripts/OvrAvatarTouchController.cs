80cdcb5436fdb0482bc579210fcc38ce7f33d809
Esteban697 <estebanjavierst@gmail.com>
2019-02-07 16:36:56 +0100

    Gesture activation with material change in objectToChange. Scale Up color change not working.

--- VisualDataRoom_VR/Assets/Oculus/Avatar/Scripts/OvrAvatarTouchController.cs
+++ VisualDataRoom_VR/Assets/Oculus/Avatar/Scripts/OvrAvatarTouchController.cs
@@ -8,18 +8,21 @@ public class OvrAvatarTouchController : MonoBehaviour
     public OVRInput.Controller controller;
     public ThumbCollider thumbCollider;
     public PalmCollider palmCollider;
-    public GameObject zoomPlane;
-    public GameObject zoomPlanePosition;
+    //public GameObject zoomPlane;
+    //public GameObject zoomPlanePosition;
     public GameObject otherController;
     public GameObject objectToChange;
     public Transform objectTransform;
     public float distanceReference = 0.3f;
     public bool zoomInOn = false;
     public bool zoomOutOn = false;
+    public bool pointing = false;
+    public OvrAvatarTouchController otherControllerScript;
+    public Renderer[] editRenderers;
 
     void Start()
     {
-        
+        editRenderers = objectToChange.GetComponentsInChildren<Renderer>();
     }
         void Update()
     {
@@ -54,9 +57,11 @@ public class OvrAvatarTouchController : MonoBehaviour
             OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, controller) > 0.7f &&
             thumbCollider.thumbsAlligned)
         {
-            zoomPlanePosition.transform.SetParent(objectToChange.transform);
-            zoomPlanePosition.transform.localPosition = (Vector3.down * objectToChange.transform.localScale.x * 1.5f);
-            zoomPlanePosition.SetActive(true);
+            //zoomPlanePosition.transform.SetParent(objectToChange.transform);
+            //zoomPlanePosition.transform.localPosition = (Vector3.down * objectToChange.transform.localScale.x * 1.5f);
+            //zoomPlanePosition.SetActive(true);
+            foreach (Renderer rend in editRenderers)
+                rend.material.color = Color.blue;
             objectTransform.localScale = objectToChange.transform.localScale;
             zoomInOn = true;
             zoomOutOn = false;
@@ -67,10 +72,10 @@ public class OvrAvatarTouchController : MonoBehaviour
                  OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, controller) > 0.7f &&
                  !zoomOutOn)
         {
-            zoomPlane.transform.localScale =
-                new Vector3((Mathf.Abs(this.transform.position.x - otherController.transform.position.x) * 0.9f),
-                    0.05f,
-                    (Mathf.Abs(this.transform.position.x - otherController.transform.position.x) * 0.9f));
+            //zoomPlane.transform.localScale =
+            //    new Vector3((Mathf.Abs(this.transform.position.x - otherController.transform.position.x) * 0.9f),
+            //        0.05f,
+            //        (Mathf.Abs(this.transform.position.x - otherController.transform.position.x) * 0.9f));
 
             var distanceBetween = (Mathf.Abs(this.transform.position.x - otherController.transform.position.x) * 0.01f);
             
@@ -81,13 +86,17 @@ public class OvrAvatarTouchController : MonoBehaviour
             if (zoomInOn == true)
             {
                 objectToChange.transform.localScale = objectTransform.localScale;
+                foreach (Renderer rend in editRenderers)
+                    rend.material.color = Color.blue;
             }
         }
         //Change gesture
         else 
         {
-            zoomPlanePosition.SetActive(false);
+            //zoomPlanePosition.SetActive(false);
             zoomInOn = false;
+            foreach (Renderer rend in editRenderers)
+                rend.material.color = Color.black;
         }
 
         //Scale Down Gesture: Detection of "corner gesture": thumb and index up with grip pressed with palms alligned
@@ -96,9 +105,12 @@ public class OvrAvatarTouchController : MonoBehaviour
             OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, controller) > 0.7f &&
             palmCollider.palmsAlligned)
         {
-            zoomPlanePosition.transform.SetParent(objectToChange.transform);
-            zoomPlanePosition.transform.localPosition = (Vector3.down * objectToChange.transform.localScale.x * 1.5f);
-            zoomPlanePosition.SetActive(true);
+            //zoomPlanePosition.transform.SetParent(objectToChange.transform);
+            //zoomPlanePosition.transform.localPosition = (Vector3.down * objectToChange.transform.localScale.x * 1.5f);
+            //zoomPlanePosition.SetActive(true);
+            foreach (Renderer rend in editRenderers)
+                rend.material.color = Color.blue;
+
             objectTransform.localScale = objectToChange.transform.localScale;
             zoomOutOn = true;
             zoomInOn = false;
@@ -109,10 +121,10 @@ public class OvrAvatarTouchController : MonoBehaviour
                  OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, controller) > 0.7f &&
                  !zoomInOn)
         {
-            zoomPlane.transform.localScale =
-                new Vector3((Mathf.Abs(this.transform.position.x - otherController.transform.position.x) * 0.9f),
-                    0.05f,
-                    (Mathf.Abs(this.transform.position.x - otherController.transform.position.x) * 0.9f));
+            //zoomPlane.transform.localScale =
+            //    new Vector3((Mathf.Abs(this.transform.position.x - otherController.transform.position.x) * 0.9f),
+            //        0.05f,
+            //        (Mathf.Abs(this.transform.position.x - otherController.transform.position.x) * 0.9f));
 
             var distanceBetween2 = (0.38f/(Mathf.Abs(this.transform.position.x - otherController.transform.position.x)))*0.001f;
 
@@ -123,13 +135,35 @@ public class OvrAvatarTouchController : MonoBehaviour
             if (zoomOutOn == true)
             {
                 objectToChange.transform.localScale = objectTransform.localScale;
+                foreach (Renderer rend in editRenderers)
+                    rend.material.color = Color.blue;
             }
         }
         // Change gesture
         else
         {
-            zoomPlanePosition.SetActive(false);
+            //zoomPlanePosition.SetActive(false);
             zoomOutOn = false;
+            foreach (Renderer rend in editRenderers)
+                rend.material.color = Color.black;
+        }
+
+        //Translate Gesture: Detection of pointing of current controller and flag up
+        if (OVRInput.Get(OVRInput.NearTouch.PrimaryThumbButtons, controller) &&
+            !OVRInput.Get(OVRInput.NearTouch.PrimaryIndexTrigger, controller) &&
+            OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, controller) > 0.7f)
+        {
+            pointing = true;
+        }
+        else
+        {
+            pointing = false;
+        }
+        // IF both controllers are pointing initiate translation of object
+        if (otherControllerScript.pointing && pointing)
+        {
+            foreach (Renderer rend in editRenderers)
+                rend.material.color = Color.blue;
         }
     }
 }
